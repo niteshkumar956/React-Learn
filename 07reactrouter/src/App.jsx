2a40f6c8fb3199d07ab7c044f915b8f943eaade9
niteshkumar956 <113203650+niteshkumar956@users.noreply.github.com>
2025-01-31 14:30:12 +0000

    07reactrouter

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ 
+ 
+
+   
+    </>
+  )
+}
+
+
+export default App
